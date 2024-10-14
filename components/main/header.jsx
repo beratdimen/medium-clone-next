8d8a4249569ab7d5461e7e8b757652106e9baf4d
Berat Dimen <beratefedimen@hotmail.com>
2024-10-14 15:31:08 +0300

    postslar sayfası yapıldı tarihleri yazdırıldı

--- components/main/header.jsx
+++ components/main/header.jsx
@@ -2,6 +2,8 @@ import { signout } from "@/app/login/action";
 import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 
+import "./header.css";
+
 export default async function MainHeader() {
   const supabase = createClient();
   const {
@@ -10,31 +12,33 @@ export default async function MainHeader() {
 
   return (
     <header>
+      <h1>LOGO</h1>
       <ul>
         <li>Anasayfa</li>
         <li>Hakkımızda</li>
+        <li>Postlar</li>
         <li>İletişim</li>
-
-        {user ? (
-          <ul>
-            <li>Hoş geldin , {user.email}</li>
-            <li>
-              <form action={signout}>
-                <button>Çıkış Yap</button>
-              </form>
-            </li>
-          </ul>
-        ) : (
-          <ul>
-            <li>
-              <Link href={"/login"}>Giriş Yap</Link>
-            </li>
-            <li>
-              <Link href={"/sign-up"}>Kayıt ol</Link>
-            </li>
-          </ul>
-        )}
       </ul>
+
+      {user ? (
+        <ul>
+          <li>Hoş geldin , {user.email}</li>
+          <li>
+            <form action={signout}>
+              <button>Çıkış Yap</button>
+            </form>
+          </li>
+        </ul>
+      ) : (
+        <ul>
+          <li>
+            <Link href={"/login"}>Giriş Yap</Link>
+          </li>
+          <li>
+            <Link href={"/sign-up"}>Kayıt ol</Link>
+          </li>
+        </ul>
+      )}
     </header>
   );
 }
