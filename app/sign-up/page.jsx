4df6bf5ea18a1e48bc83aa342bb0d82b15ed11ba
Berat Dimen <beratefedimen@hotmail.com>
2024-10-17 00:02:53 +0300

    sign up forma ekleme yapıldı

--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -1,30 +1,23 @@
 import { signup } from "../login/action";
-
+import "./signUp.css";
 export default function SignUpPage() {
   return (
-    <form>
-      <label htmlFor="firstName">Ad:</label>
-      <input
-        id="firstName"
-        name="firstName"
-        type="text"
-        required
-        placeholder="Ad"
-      />
-      <label htmlFor="lastName">Soyad:</label>
+    <div className="loginContainer">
+      <h2>Sign Up Form</h2>
 
-      <input
-        id="lastName"
-        name="lastName"
-        type="text"
-        required
-        placeholder="Soyad"
-      />
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={signup}>Sign up</button>
-    </form>
+      <form>
+        <input id="firstName" name="firstName" type="text" placeholder="Ad" />
+
+        <input id="lastName" name="lastName" type="text" placeholder="Soyad" />
+        <input id="email" name="email" type="email" placeholder="E-Posta" />
+        <input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="******"
+        />
+        <button formAction={signup}>Sign up</button>
+      </form>
+    </div>
   );
 }
